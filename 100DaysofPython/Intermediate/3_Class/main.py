2f5236ebd108f825a40e2c55e08b9981215d7ebb
Bejnack <cristopherdorner@gmail.com>
2024-07-15 15:32:52 +0300

    class

---
+++ 100DaysofPython/Intermediate/3_Class/main.py
@@ -0,0 +1,17 @@
+class User:
+
+    def __init__(self):
+        print("new user being created....")
+
+
+user_1 = User()
+user_1.id = "001"
+user_1.username = "Cris"
+
+print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Beji"
+
+print(user_2.username)
