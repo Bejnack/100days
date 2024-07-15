7516311d61307a930cd27df40b2914ba50eca372
Bejnack <cristopherdorner@gmail.com>
2024-07-15 23:22:22 +0300

    Done day 17 OOP + Class creation + QuizzGame

--- 100DaysofPython/Intermediate/3_Class/QuizGame/main.py
+++ 100DaysofPython/Intermediate/3_Class/QuizGame/main.py
@@ -1,12 +1,26 @@
+"""_summary_
+main loop
+"""
+
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+
+
+print(quiz.still_has_questions())
+
+while quiz.still_has_questions():
+    quiz.next_question()
+    
+print(f"""You've completed the quizz!
+Your final score is {quiz.score}/{quiz.question_number}.""")
