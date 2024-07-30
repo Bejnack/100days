414716dee06ec5ecd06886667348a195c14360db
Bejnack <cristopherdorner@gmail.com>
2024-07-30 17:57:21 +0300

    done day 19

---
+++ 100DaysofPython/Intermediate/5_day/turtle_race/main.py
@@ -0,0 +1,43 @@
+from turtle import Turtle, Screen
+import random
+
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [165, 99, 33, -33, -99, -165]
+all_turtles = []
+
+
+course = Turtle()
+course.penup()
+course.goto(x=210,y=200)
+course.pendown()
+course.goto(x=210,y=-200)
+
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 210:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+        rand_dist = random.randint(0,10)
+        turtle.forward(rand_dist)
+
+screen.exitonclick()
