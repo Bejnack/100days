bb39c9159bc1f2e84d8e4486c65c469512287c32
Bejnack <cristopherdorner@gmail.com>
2024-09-11 15:03:27 +0300

    start of 192

--- 100DaysofPython/Intermediate/10_Weather_Channel/main.py
+++ 100DaysofPython/Intermediate/10_Weather_Channel/main.py
@@ -19,4 +19,20 @@
 
 import pandas
 data = pandas.read_csv(r"Intermediate\10_Weather_Channel\weather_data.csv")
-print(data)
+#print(data["temp"])
+data_dict = data.to_dict()
+#print(data_dict)
+temp_list = data["temp"].to_list()
+# # avr = sum(temp_list) / len(temp_list)
+# # print(avr)
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+#print(monday)
+monday_temp = monday.temp[0]
+fahrenheit_temp = (monday_temp * 9/5) + 32
+print(fahrenheit_temp)
+
+#formaula = (0°C × 9/5) + 32 = 32°F
